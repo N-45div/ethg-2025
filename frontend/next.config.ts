1f55472287ce7fd71bbfb6450235b4c64e3158d0
N DIVIJ <ndivij2004@gmail.com>
2025-10-23 21:35:31 +0000

    feat: config state

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,19 +1,16 @@
 import type { NextConfig } from "next";
-import path from "path";
 
 const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
   },
-  outputFileTracingRoot: path.join(__dirname, "../../"),
   webpack: (config) => {
     config.resolve = config.resolve || {};
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
       "@react-native-async-storage/async-storage": false,
       "@react-native-async-storage/async-storage/jest/async-storage-mock": false,
-      "pino-pretty": false,
     };
     return config;
   },
